c7ad710990b0e0441f68bb092c712e288a6d4b07
1193700079 <l193700079@qq.com>
2019-08-28 16:47:57 +0800

    使用mybatis 逆向工程

--- src/main/java/life/hblg/community/CommunityApplication.java
+++ src/main/java/life/hblg/community/CommunityApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "life.hblg.community.mapper")
+
 @SpringBootApplication
+@MapperScan("life.hblg.community.mapper")
+
 public class CommunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/life/hblg/community/model/User.java
+++ src/main/java/life/hblg/community/model/User.java
@@ -1,22 +1,83 @@
 package life.hblg.community.model;
 
-import lombok.Data;
-import org.springframework.stereotype.Component;
-
-//在数据库的数据交换中 存放在model中
-@Component
-@Data  //lombok插件 自动生成 get set toSting方法
 public class User {
-
-
     private Integer id;
+
     private String accountId;
+
     private String name;
+
     private String token;
+
     private Long gmtCreate;
+
     private Long gmtModify;
-    private String avatarUrl; //图片
+
     private String bio;
 
+    private String avatarUrl;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId == null ? null : accountId.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token == null ? null : token.trim();
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModify() {
+        return gmtModify;
+    }
+
+    public void setGmtModify(Long gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio == null ? null : bio.trim();
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl == null ? null : avatarUrl.trim();
+    }
 }
